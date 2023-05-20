dbff1bacc59411c3eaf7745b6b798d4789a3c0e0
aaron tanjaya <aaront.tjy@gmail.com>
2023-05-20 18:58:38 +0700

    feat: add client code to call streaming rpc

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -4,20 +4,31 @@ import (
 	"context"
 	"grpc-crash-course/client"
 	pb "grpc-crash-course/todoProto"
+	"io"
 	"log"
 )
 
 func main() {
 	client := client.ClientTodo()
-	item, err := client.CreateTodo(context.Background(), &pb.TodoItem{
-		Text: "Makan",
-		Id:   1,
-	})
-	if err != nil {
-		log.Fatal("error: ", err)
+
+	todos := []string{
+		"Makan",
+		"Minum",
+		"Tidur",
+		"Baca",
 	}
 
-	log.Printf("item created: %v \n", item)
+	for i, act := range todos {
+		item, err := client.CreateTodo(context.Background(), &pb.TodoItem{
+			Text: act,
+			Id:   int32(i),
+		})
+		if err != nil {
+			log.Fatal("error: ", err)
+		}
+
+		log.Printf("item created: %v \n", item)
+	}
 
 	list, err := client.ReadTodos(context.Background(), &pb.Void{})
 	if err != nil {
@@ -25,4 +36,28 @@ func main() {
 	}
 
 	log.Printf("list of items: %v", list)
+
+	stream, err := client.ReadTodosStream(context.Background(), &pb.Void{})
+	if err != nil {
+		log.Fatal("error: ", err)
+	}
+
+	done := make(chan bool)
+
+	go func() {
+		for {
+			resp, err := stream.Recv()
+			if err == io.EOF {
+				done <- true
+				return
+			}
+			if err != nil {
+				log.Fatal("failed streaming")
+			}
+			log.Printf("Stream result : %v", resp)
+		}
+	}()
+
+	<-done
+	log.Println("Done")
 }
